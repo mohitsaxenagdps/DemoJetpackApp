f5d36466f309b3c1300959b8ff2fd95f2a224260
mohit <mohitgdps@gmail.com>
2021-02-19 15:35:51 +0530

    View Model Factory added to initialize starting total

--- app/src/main/java/com/example/demousingjetpack/MainActivity.kt
+++ app/src/main/java/com/example/demousingjetpack/MainActivity.kt
@@ -11,11 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModelFactory = MainActivityViewModelFactory(10.5)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
         binding.resultTxtView.text = viewModel.getTotal().toString()
 
--- app/src/main/java/com/example/demousingjetpack/MainActivityViewModel.kt
+++ app/src/main/java/com/example/demousingjetpack/MainActivityViewModel.kt
@@ -2,9 +2,13 @@ package com.example.demousingjetpack
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel(startingTotal: Double): ViewModel() {
 
     private var total = 0.0
+
+    init {
+        total = startingTotal
+    }
 
     fun setTotal(input: Double){
         total += input
