a8cc5e0f1a4a970a9fb28110c3e4010280b1bb9e
mohit <mohitgdps@gmail.com>
2021-02-19 16:33:38 +0530

    Live Data added to observe the total variable and update ui automatically

--- app/src/main/java/com/example/demousingjetpack/MainActivity.kt
+++ app/src/main/java/com/example/demousingjetpack/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.demousingjetpack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.demousingjetpack.databinding.ActivityMainBinding
 import java.lang.NumberFormatException
@@ -19,13 +20,14 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(10.5)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
-        binding.resultTxtView.text = viewModel.getTotal().toString()
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTxtView.text = it.toString()
+        })
 
         binding.apply {
             addBtn.setOnClickListener {
                 try {
                     viewModel.setTotal(editTextLayout.editText?.text.toString().toDouble())
-                    resultTxtView.text = viewModel.getTotal().toString()
                 } catch (e: NumberFormatException) {
                     editText.error = "Number Required!"
                 }
--- app/src/main/java/com/example/demousingjetpack/MainActivityViewModel.kt
+++ app/src/main/java/com/example/demousingjetpack/MainActivityViewModel.kt
@@ -1,19 +1,22 @@
 package com.example.demousingjetpack
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Double): ViewModel() {
 
-    private var total = 0.0
+    private var total: MutableLiveData<Double> = MutableLiveData(0.0)
+
+    val totalData: LiveData<Double>
+    get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
     fun setTotal(input: Double){
-        total += input
+        total.value = total.value?.plus(input)
     }
-
-    fun getTotal() = total
 
 }
